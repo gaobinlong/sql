9fbb299c53fe666c3cd33926acbfc20d23e3b99b
gaobinlong <gbinlong@amazon.com>
2023-11-27 10:42:18 +0800

    Transform response
    Signed-off-by: gaobinlong <gbinlong@amazon.com>

--- client/src/main/java/org.opensearch.sql.client/SqlNodeClient.java
+++ client/src/main/java/org.opensearch.sql.client/SqlNodeClient.java
@@ -2,18 +2,32 @@ package org.opensearch.sql.client;
 
 import org.opensearch.client.Client;
 import org.opensearch.core.action.ActionListener;
+import org.opensearch.core.action.ActionResponse;
 import org.opensearch.sql.ppl.transport.PPLQueryAction;
 import org.opensearch.sql.ppl.transport.TransportPPLQueryRequest;
 import org.opensearch.sql.ppl.transport.TransportPPLQueryResponse;
 
+import java.util.function.Function;
+
 public class SqlNodeClient implements SqlClient {
     Client client;
+
     @Override
     public void pplQuery(TransportPPLQueryRequest request, ActionListener<TransportPPLQueryResponse> listener) {
-        client.execute(
-            PPLQueryAction.INSTANCE,
-            request,
-            ActionListener.wrap(listener::onResponse, listener::onFailure)
-        );
+        client.execute(PPLQueryAction.INSTANCE, request, getListener(listener));
+    }
+
+    private ActionListener<TransportPPLQueryResponse> getListener(
+        ActionListener<TransportPPLQueryResponse> listener
+    ) {
+        return wrapActionListener(listener, TransportPPLQueryResponse::fromActionResponse);
+    }
+
+    private <T extends ActionResponse> ActionListener<T> wrapActionListener(
+        final ActionListener<T> listener, final Function<ActionResponse, T> recreate
+    ) {
+        return ActionListener.wrap(r -> {
+            listener.onResponse(recreate.apply(r));
+        }, listener::onFailure);
     }
 }
--- ppl/src/main/java/org/opensearch/sql/ppl/transport/TransportPPLQueryResponse.java
+++ ppl/src/main/java/org/opensearch/sql/ppl/transport/TransportPPLQueryResponse.java
@@ -8,14 +8,22 @@ package org.opensearch.sql.ppl.transport;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.opensearch.core.action.ActionResponse;
+import org.opensearch.core.common.io.stream.InputStreamStreamInput;
+import org.opensearch.core.common.io.stream.OutputStreamStreamOutput;
 import org.opensearch.core.common.io.stream.StreamInput;
 import org.opensearch.core.common.io.stream.StreamOutput;
+import org.opensearch.core.xcontent.ToXContentObject;
+import org.opensearch.core.xcontent.XContentBuilder;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.UncheckedIOException;
 
 @RequiredArgsConstructor
-public class TransportPPLQueryResponse extends ActionResponse {
-    @Getter private final String result;
+public class TransportPPLQueryResponse extends ActionResponse implements ToXContentObject {
+    @Getter
+    private final String result;
 
     public TransportPPLQueryResponse(StreamInput in) throws IOException {
         super(in);
@@ -25,6 +33,32 @@ public class TransportPPLQueryResponse extends ActionResponse {
     @Override
     public void writeTo(StreamOutput out) throws IOException {
         out.writeString(result);
+    }
+
+    @Override
+    public XContentBuilder toXContent(XContentBuilder builder, Params params) throws IOException {
+        builder.startObject();
+        if (result != null) {
+            builder.field("result", result);
+        }
+        return builder.endObject();
+    }
+
+    public static TransportPPLQueryResponse fromActionResponse(ActionResponse actionResponse) {
+        if (actionResponse instanceof TransportPPLQueryResponse) {
+            return (TransportPPLQueryResponse) actionResponse;
+        }
+        try (
+            ByteArrayOutputStream baos = new ByteArrayOutputStream(); OutputStreamStreamOutput osso = new OutputStreamStreamOutput(baos)
+        ) {
+            actionResponse.writeTo(osso);
+            try (StreamInput input = new InputStreamStreamInput(new ByteArrayInputStream(baos.toByteArray()))) {
+                return new TransportPPLQueryResponse(input);
+            }
+        } catch (IOException e) {
+            throw new UncheckedIOException("failed to parse ActionResponse into TransportPPLQueryResponse", e);
+        }
+
     }
 }
 
